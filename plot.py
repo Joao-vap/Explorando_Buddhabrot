fe240138ba8cc2b97c52e963192263f804ffa372
Edgar Macena <42724363+eru-iluv@users.noreply.github.com>
2020-12-12 00:44:54 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -8,18 +8,34 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 argumentos = sys.argv
 
-# argumentos[0] nome do arquivo
-# argumentos[1] nome do arquivo com valores
+# argumentos[0] nome desse arquivo
+# argumentos[1] nome do arquivo que contém os valores
+
+## Esse trecho seria escrito de maneira infinitamente mais legível se utilizasse 
+## regular expressions, não tenho paciência pra isso, mas quem quiser mudar não 
+## terá dificuldades
+ 
+npontos = argumentos[1].split("-")[1][1:]
+
 
 ## "pot" vai definir o contraste nas imagens a serem criadas [0 < pot <= 1]
 pot = [0.2, 0.4, 0.6, 0.8]
 
+## Deixamos a escolha basica de cores a cargo de quem roda o programa
+cmap = input("cmap:")
+if cmap == "":
+    cmap = "inferno"
 for p in pot:
     ## Carrega-se o texto do outro arquivo
     imagem = (np.loadtxt(argumentos[1]) + 1)**p
 
     ## Resta definir as cores e abrir a imagem
-    plt.imshow(imagem, cmap="inferno")
-    plt.savefig('inferno-npontos.10000000_resolucao.1024x1024' + '_p.' + str(p) + '.png')
+    try: 
+        plt.imshow(imagem, cmap=cmap)
+    except ValueError:
+        cmap = "inferno"
+        plt.imshow(imagem, cmap=cmap)        
+    plt.savefig('{}-npontos.{}.resolucao.1024x1024'.format(cmap,npontos) + '_p.' + str(p) + '.png')
